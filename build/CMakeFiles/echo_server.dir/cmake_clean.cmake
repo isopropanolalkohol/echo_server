2719d0a946a7d88c44b9c672ee6cb8187757ac03
Joshua <joshuageissler.pharworlds@gmail.com>
2025-06-03 23:57:59 +0200

    IDK

---
+++ build/CMakeFiles/echo_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo_server.dir/src/main.c.o"
+  "CMakeFiles/echo_server.dir/src/main.c.o.d"
+  "echo_server"
+  "echo_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/echo_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
